f97f29a45eb905bcbb4152b327260548d2fd61dc
rio-5 <154059238+rio-5@users.noreply.github.com>
2024-04-06 11:23:07 +0800

    Create gemini.js

---
+++ script/gemini.js
@@ -0,0 +1,43 @@
+module.exports.config = {
+  name: 'gemini',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['gpt', 'openai'],
+  description: "An AI command powered by GPT-4",
+  usage: "gemini [promot]",
+  credits: 'james',
+  cooldown: 3,
+};
+module.exports.run = async function ({
+  api,
+  event,
+  args
+}) {
+  const axios = require('axios');
+  const { threadID, messageID } = event;
+  const prompt = args.join(" ");
+  let uid = event.senderID;
+  
+  
+  if (!prompt) {
+    api.sendMessage(`please put a prompt!`, threadID, messageID);
+    return;
+  };
+  
+  api.sendMessage(`[🔍] searching for "${prompt}"`, threadID, messageID);
+  
+  try {
+    let apiUrl = "https://gemini-api.replit.app";
+    
+    if (event.type == "message_reply") {
+      if (event.messageReply.attachments[0]?.type == "photo") {
+        url = encodeURIComponent(event.messageReply.attachments[0].url);
+        const res = await axios.get(apiUrl + "/gemini?prompt=" + prompt + "&uid=" + uid + "&url=" + url);
+        return api.sendMessage(res.data.gemini, threadID, messageID);
+      }
+    };
+  } catch (e) {
+    api.sendMessage("error", threadID, messageID)
+  }
+}
